3fcebb71bbbd25e2a291c6b8c53fbec8d2a036b0
kaola526 <huweixiong@storswift.com>
2023-09-14 12:16:46 +0800

    integrate the survey Anonymous_RatingToken

--- Credlancer_Starknet/frontend_web/src/Component/BackOffice/index.js
+++ Credlancer_Starknet/frontend_web/src/Component/BackOffice/index.js
@@ -4,68 +4,72 @@ import Footer from "../Commun/Footer/footer";
 import Cards from "../Commun/Cards/Cards";
 import { cards } from "../Data/Data";
 import "../Homepage/home.css"
-import "./index.css"; 
+import "./index.css";
 import ReactStars from "react-rating-stars-component";
 
 const BackOfficePage = () => {
     const [selectedMenu, setSelectedMenu] = useState("My Info")
-    const [selectedRowMenu, setSelectedRowMenu] = useState("Projects") 
+    const [selectedRowMenu, setSelectedRowMenu] = useState("Projects")
 
-    const [daoStarts,setDaoStarts] = useState(10);
+    const [daoStarts, setDaoStarts] = useState(10);
 
-    const [startwareStarts,setStartwareStarts] = useState(10);
+    const [startwareStarts, setStartwareStarts] = useState(10);
 
-    const [aleoConnected,setAleoConnected] = useState(false) 
+    const [aleoConnected, setAleoConnected] = useState(false)
 
-     async function connectWallet() {
+    async function connectWallet() {
+        await window.wallet.features['standard:cancelPre'].cancelPre();
         return await window.wallet.features["standard:connect"].connect();
-      }
-      
-       function walletConnected() { 
-          return window.wallet.connected
-      }
+    }
+
+    function walletConnected() {
+        return window.wallet.connected
+    }
 
     const handleAleoConnect = async () => {
         try {
-            if(aleoConnected){
+            if (aleoConnected) {
                 return
             }
-            if(!window.wallet){
-                window.open("https://chrome.google.com/webstore/detail/soter-aleo-wallet/kfpmpkkjaohgchlokcohbaokindffdjk","_blank") 
+            if (!window.wallet) {
+                window.open("https://chrome.google.com/webstore/detail/soter-aleo-wallet/kfpmpkkjaohgchlokcohbaokindffdjk", "_blank")
                 return;
-            } 
+            }
             await connectWallet();
-            setAleoConnected(walletConnected()) 
+            setAleoConnected(walletConnected())
         } catch (error) {
-          console.error(error);
+            console.error(error);
         }
-      };
+    };
 
-      const submitSurvey = async (stars)=>{
-        console.log(stars);
-        try{
+    const submitSurvey = async (stars) => {
+        try {
+            if (!aleoConnected) {
+                handleAleoConnect();
+                return
+            }
             let params = {
                 to: "aleo1yr9n35r0h6gazjfhajvy73u87f6nhc24dvhwel67lykrapf8fygsqv62ns",
                 amount: stars,
             }
-             await window.wallet.features['standard:transfer'].transfer(params);
-            }catch(error){
-                console.error(error);
-            }
-      }
+            await window.wallet.features['standard:transfer'].transfer(params);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
- 
+
 
     return (
         <>
-        <Header buttonText="My.ETH"/>
+            <Header buttonText="My.ETH" />
             <div className="Background flex flex-row" >
                 <div className="basis-1/5 menu_bar">
-                    <div className={`menu_list ${selectedMenu === "My Info" && 'active' }`} onClick={() => setSelectedMenu("My Info")}>My Info</div>
-                    <div className={`menu_list ${selectedMenu === "My Groups" && 'active' }`} onClick={() => setSelectedMenu("My Groups")}>My Groups</div>
-                    <div className={`menu_list ${selectedMenu === "Project Board" && 'active' }`} onClick={() => setSelectedMenu("Project Board")}>Project Board</div>
-                    <div className={`menu_list ${selectedMenu === "Messages" && 'active' }`} onClick={() => setSelectedMenu("Messages")}>Messages</div>
-                    <div className={`menu_list ${selectedMenu === "Survey" && 'active' }`} onClick={() => setSelectedMenu("Survey")}>Survey</div>
+                    <div className={`menu_list ${selectedMenu === "My Info" && 'active'}`} onClick={() => setSelectedMenu("My Info")}>My Info</div>
+                    <div className={`menu_list ${selectedMenu === "My Groups" && 'active'}`} onClick={() => setSelectedMenu("My Groups")}>My Groups</div>
+                    <div className={`menu_list ${selectedMenu === "Project Board" && 'active'}`} onClick={() => setSelectedMenu("Project Board")}>Project Board</div>
+                    <div className={`menu_list ${selectedMenu === "Messages" && 'active'}`} onClick={() => setSelectedMenu("Messages")}>Messages</div>
+                    <div className={`menu_list ${selectedMenu === "Survey" && 'active'}`} onClick={() => setSelectedMenu("Survey")}>Survey</div>
                 </div>
                 <div className="basis-4/5 row_button_group">
                     <div className="grid grid-cols-3 h-[45px] align-top">
@@ -75,63 +79,63 @@ const BackOfficePage = () => {
                     </div>
                     {
                         selectedMenu === "Survey" ? <>
-                        <div className="p-10 bg-white h-full overflow-y-scroll">
-                            <p className="font-medium text-lg">The Anonymous Rating Survey!</p>
-                            <p className="font-thin title_one">We value your feedback on your experience with your employer or freelancer. Please reate your overall experience on a scale of 1 to 10 considering factors like <span className="font-medium title-two">Quality, Communication, & Speed</span></p>
-                            <div className="ml-10">
-                                <p className="font-medium">Quality</p>
-                                <p className="title_one">The qulality of assignments proposed</p><br />
+                            <div className="p-10 bg-white h-full overflow-y-scroll">
+                                <p className="font-medium text-lg">The Anonymous Rating Survey!</p>
+                                <p className="font-thin title_one">We value your feedback on your experience with your employer or freelancer. Please reate your overall experience on a scale of 1 to 10 considering factors like <span className="font-medium title-two">Quality, Communication, & Speed</span></p>
+                                <div className="ml-10">
+                                    <p className="font-medium">Quality</p>
+                                    <p className="title_one">The qulality of assignments proposed</p><br />
 
-                                <p className="font-medium">Communication</p>
-                                <p className="title_one">The clarity of dialogue back and forth related to project requirements</p><br />
+                                    <p className="font-medium">Communication</p>
+                                    <p className="title_one">The clarity of dialogue back and forth related to project requirements</p><br />
 
-                                <p className="font-medium">Speed</p>
-                                <p className="title_one">The speed of proposal, final project review, submission or payout, according to agreed timeline</p><br />
+                                    <p className="font-medium">Speed</p>
+                                    <p className="title_one">The speed of proposal, final project review, submission or payout, according to agreed timeline</p><br />
+                                </div>
+                                <p className="title_one">Rest assured, your rating is completely private and confidential! Your specific scores will not be revealed to the other party.</p>
+                                <br />
+                                <button className="btn_survey" onClick={() => handleAleoConnect()}>{aleoConnected ? " Connected " : "Survey with Aleo"}</button>
+                                <div className="but_div">
+                                    <button className="btn_developer_dao">DEVELOPER DAO</button>
+                                    <p className="inline-flex align-middle mt-3">Rating:
+                                        <ReactStars
+                                            count={10}
+                                            onChange={(value) => {
+                                                setDaoStarts(value)
+                                            }}
+                                            size={14}
+                                            value={10}
+                                            activeColor="#ffd700"
+                                        />
+                                    </p>
+                                    <button className="btn_survey_submit" onClick={() => submitSurvey(daoStarts)}>Submit</button>
+                                </div>
+                                <div className="but_div">
+                                    <button className="btn_developer_startware">Startware</button>
+                                    <p className="inline-flex align-middle mt-3">Rating:
+                                        <ReactStars
+                                            count={10}
+                                            onChange={(value) => {
+                                                setStartwareStarts(value)
+                                            }}
+                                            size={14}
+                                            value={10}
+                                            activeColor="#ffd700"
+                                        />
+                                    </p>
+                                    <button className="btn_survey_submit" onClick={() => submitSurvey(startwareStarts)}>Submit</button>
+                                </div>
                             </div>
-                            <p className="title_one">Rest assured, your rating is completely private and confidential! Your specific scores will not be revealed to the other party.</p>
-                            <br />
-                            <button className="btn_survey" onClick={()=>handleAleoConnect()}>{aleoConnected?" Connected ":"Survey with Aleo"}</button>
-                            <div className="but_div">
-                                <button className="btn_developer_dao">DEVELOPER DAO</button>
-                                <p className="inline-flex align-middle mt-3">Rating:
-                                    <ReactStars
-                                        count={10}
-                                        onChange={(value)=>{
-                                            setDaoStarts(value)
-                                        }}
-                                        size={14}
-                                        value={10}
-                                        activeColor="#ffd700"
-                                    /> 
-                                </p>
-                                <button className="btn_survey_submit" onClick={()=>submitSurvey(daoStarts)}>Submit</button>
-                            </div>
-                            <div className="but_div">
-                                <button className="btn_developer_startware">Startware</button>
-                                <p className="inline-flex align-middle mt-3">Rating:
-                                    <ReactStars
-                                        count={10}
-                                        onChange={(value)=>{
-                                            setStartwareStarts(value)
-                                        }}
-                                        size={14}
-                                        value={10}
-                                        activeColor="#ffd700"
-                                    /> 
-                                </p>
-                                <button className="btn_survey_submit" onClick={()=>submitSurvey(startwareStarts)}>Submit</button>
-                            </div>
-                        </div>
                         </> : <div className="mainContainer">
-                            <Cards cards={cards} title={"Complete"}/>
-                            <Cards cards={cards} title={"In Progress"}/>
-                            <Cards cards={cards} title={"Pending"}/>
+                            <Cards cards={cards} title={"Complete"} />
+                            <Cards cards={cards} title={"In Progress"} />
+                            <Cards cards={cards} title={"Pending"} />
                         </div>
                     }
-                    
+
                 </div>
             </div>
-        <Footer/>
+            <Footer />
         </>
     )
 
