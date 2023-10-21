be5b39d7c0120d27988d7ba9afb09ce5a450c675
Emanherawy <eman.herawy@gmail.com>
2023-10-22 02:57:12 +0300

    add deployment script for sender

---
+++ credLancer_cross_chain/script/deployer.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import "./Helper.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/token/ERC20/IERC20.sol";
+ import {QuestControllerSender} from "../src/cross-chain/QuestControllerSender.sol";
+
+contract Deployer is Script, Helper {
+
+   function deploySender(
+           SupportedNetworks destination,
+          SupportedNetworks source
+      
+     )external returns ( QuestControllerSender sender)  {
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+           ( address sourceRouter, address linkToken, address weth_, ) = getConfigFromNetwork(
+            source
+        );
+        //  IERC20(linkToken).transfer(sender,fees);
+        (, , , uint64 destinationChainId) = getConfigFromNetwork(destination);
+                /**    address router_, address link ,address payable weth_, uint64 destinationChainSelector_ */
+
+ sender = new  QuestControllerSender(sourceRouter, linkToken,payable(weth_),destinationChainId);
+        IERC20(weth_).approve(address(sender), 1 ether);
+
+ 
+     // send some link token to the contract to be used to pay fees
+     IERC20(linkToken).transfer(address(sender),30 ether);
+
+        console.log(
+            " your contract is deployed at address: ",address(sender)  );
+        
+
+        vm.stopBroadcast();
+         
+    }
+}
+    
