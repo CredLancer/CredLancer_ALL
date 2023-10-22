141ad3ef886babfed41662cc2eb6bc8d6a652654
Emanherawy <eman.herawy@gmail.com>
2023-10-22 03:35:02 +0300

    draft deployment script function for quest in dest chain

--- credLancer_cross_chain/script/deployer.sol
+++ credLancer_cross_chain/script/deployer.sol
@@ -6,9 +6,13 @@ import "./Helper.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/token/ERC20/IERC20.sol";
+ import {OrganizationController} from "../src/OrganizationController.sol";
+ import {Credential} from "../src/Credential.sol";
+ import {QuestController} from "../src/QuestController.sol";
  import {QuestControllerSender} from "../src/cross-chain/QuestControllerSender.sol";
 
 contract Deployer is Script, Helper {
+
 
    function deploySender(
            SupportedNetworks destination,
@@ -25,12 +29,12 @@ contract Deployer is Script, Helper {
         (, , , uint64 destinationChainId) = getConfigFromNetwork(destination);
                 /**    address router_, address link ,address payable weth_, uint64 destinationChainSelector_ */
 
- sender = new  QuestControllerSender(sourceRouter, linkToken,payable(weth_),destinationChainId);
-        IERC20(weth_).approve(address(sender), 1 ether);
+    sender = new  QuestControllerSender(sourceRouter, linkToken,payable(weth_),destinationChainId);
+     IERC20(linkToken).approve(address(sender), 1 ether);
 
  
-     // send some link token to the contract to be used to pay fees
-     IERC20(linkToken).transfer(address(sender),30 ether);
+    //  send some link token to the contract to be used to pay fees
+      IERC20(linkToken).transfer(address(sender),30 ether);
 
         console.log(
             " your contract is deployed at address: ",address(sender)  );
@@ -38,6 +42,23 @@ contract Deployer is Script, Helper {
 
         vm.stopBroadcast();
          
+    }
+// QuestControllerContract deployment script 
+   function deployReceiver(
+       SupportedNetworks destination,
+       SupportedNetworks source
+     
+    ) external returns ( QuestController receiver) {
+
+
+
+        uint256 senderPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(senderPrivateKey);
+         
+ 
+        vm.stopBroadcast();
+
+        
     }
 }
     
